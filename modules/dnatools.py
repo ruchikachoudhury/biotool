60b3683fee492df68030485bb0cbe5804ae92d1e
Ruchika Choudhury <thisispihuxd@gmail.com>
2026-06-18 13:22:41 +0530

    3rd commit

--- modules/dnatools.py
+++ modules/dnatools.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from Bio.Seq import Seq
+st.write("DNA Sequence Analysis Tool")
 sequence=st.text_input("Enter a DNA sequence:", "ATGCGTACGTTAGC")
 if st.button("Analyze"):
     st.write("Length of the sequence:", len(sequence))
+    st.write("complement of the sequence:", Seq(sequence).complement())
+    st.write("transcription of the sequence:", Seq(sequence).transcribe())
+    st.write("translation of the sequence:", Seq(sequence).translate())
