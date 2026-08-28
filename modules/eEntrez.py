7c16a1198e15d7dc151f3e9852f36eda255f79d3
Ruchika Choudhury <thisispihuxd@gmail.com>
2026-08-28 19:47:41 +0530

    Enhance fetch_gene_info with comments
    Added comments for clarity on data fetching and processing.

--- modules/eEntrez.py
+++ modules/eEntrez.py
@@ -1,12 +1,12 @@
 from Bio import Entrez
 def fetch_gene_info(gene_id, email, dbtype):
     Entrez.email = email
-    handle = Entrez.efetch(db=dbtype, id=gene_id, retmode="xml")
-    records = Entrez.read(handle)
-    handle.close()
+    handle = Entrez.efetch(db=dbtype, id=gene_id, retmode="xml")#storing the data and fetching data from entrez
+    records = Entrez.read(handle)#putting all the results in records
+    handle.close()#terminate connection to database
     
     if records:
-        gene_info = records[0]
+        gene_info = records[0]#using gene info for getting info and storing it in various variables so that it can be returned to the user
         gene_name = gene_info.get("Entrezgene_gene", {}).get("Gene-ref", {}).get("Gene-ref_locus", "")
         gene_description = gene_info.get("Entrezgene_summary", "")
         gene_organism = gene_info.get("Entrezgene_source", {}).get("BioSource", {}).get("BioSource_org", {}).get("Org-ref", {}).get("Org-ref_taxname", "")
