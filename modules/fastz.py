f15d7f8daffc55b2f672b4f1d4090b920fb3c70f
Ruchika Choudhury <thisispihuxd@gmail.com>
2026-08-28 22:01:30 +0530

    Clarify id assignment in read_fasta function
    Added a comment to clarify the purpose of id assignment.

--- modules/fastz.py
+++ modules/fastz.py
@@ -4,7 +4,7 @@ def read_fasta(fasta_file):
     sequences = list(SeqIO.parse(fasta_file, "fasta"))
     if fasta_file is not None:
      for seq in sequences:
-        id=seq.id
+        id=seq.id#using predefined functions to parse for and store respective information in variables
         description=seq.description
         accession=seq.annotations
         sequence=seq.seq
