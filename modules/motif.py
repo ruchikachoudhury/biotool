98bdcf6a4230cc0c011885486097d661d9d85289
Ruchika Choudhury <thisispihuxd@gmail.com>
2026-08-28 22:21:13 +0530

    Enhance find_motif_in_fasta with comments
    Added comments to clarify function purpose and logic.

--- modules/motif.py
+++ modules/motif.py
@@ -1,8 +1,8 @@
 from Bio import SeqIO
-def find_motif_in_fasta(fasta_file, motif):
-    sequences =SeqIO.parse(fasta_file, "fasta")
-    sequences_with_motif = []
-    for record in sequences:
+def find_motif_in_fasta(fasta_file, motif):#bringing arguments such as the fasta file and the motif or the sepcific region you wanna look for
+    sequences =SeqIO.parse(fasta_file, "fasta")#parsing through the fasta file
+    sequences_with_motif = []#initializing 
+    for record in sequences:#using loop to look through the fasta with the specific motif given
         if motif in record.seq:
-            sequences_with_motif.append(record)
+            sequences_with_motif.append(record)#stores if found
     return sequences_with_motif
